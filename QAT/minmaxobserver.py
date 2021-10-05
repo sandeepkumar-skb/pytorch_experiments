c20f145d793277b55111c030ad10beac6ecf89ca
Sandeep Kumar Behera <64504172+sandeepkumar-skb@users.noreply.github.com>
2021-10-04 19:21:08 -0700

    Update minmaxobserver.py

--- QAT/minmaxobserver.py
+++ QAT/minmaxobserver.py
@@ -6,4 +6,11 @@ class MinMaxObserver(torch.quantization.MinMaxObserver):
 
     def forward(self, x_orig):
         if not self.training: return x_orig
-        return super().forward(x_orig)
+        x = x_orig.detach()  # avoid keeping autograd tape
+        x = x.to(self.min_val.dtype)
+        min_val_cur, max_val_cur = torch._aminmax(x)
+        min_val = torch.min(min_val_cur, self.min_val)
+        max_val = torch.max(max_val_cur, self.max_val)
+        self.min_val.copy_(min_val)
+        self.max_val.copy_(max_val)
+        return x_orig
